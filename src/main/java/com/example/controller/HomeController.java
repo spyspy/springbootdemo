950ef387a55392164a6e17376c26c2e3368ff56e
spyspy <spyhopping101@gmail.com>
2023-05-25 22:12:41 +0800

    update

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,18 +1,30 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class HomeController {
 
+    @Value("${superman.name}")
+    private String supername;
+
     // 最快速的建立MVC方式
-    @RequestMapping("/")
+    @RequestMapping(value="/", method = RequestMethod.GET)
     String home() {
-        return "Hello World!";
+        System.out.println("Super Name:"+supername);
+//        return "Hello World!";
+        return "index";
     }
 
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model){
+        return "login";
+    }
 }
